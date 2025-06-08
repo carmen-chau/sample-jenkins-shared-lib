c47c3514e5e070742c0d491169af620f39cc3091
carmen-chau <carmenjchau@gmail.com>
2025-06-08 13:02:07 -0400

    added another shared file

--- vars/logEnvVariable.groovy
+++ vars/logEnvVariable.groovy
@@ -1,3 +1,16 @@
-void call(String envVariableName) {
-    echo "Inside the helper script, the value of ${envVariableName} is: ${env[envVariableName]}"
+void call(String gitURLVarName, String buildStageVarName) {
+    try {
+
+        // Variable reassignment, logging values for verification
+        def gitRepoName = env[gitUrlVarName]
+        def buildStageName = env[buildStageVarName]
+        echo "Inside the helper script, the value of ${gitURLVarName} is: ${gitRepoName}"
+        echo "Inside the helper script, the value of ${buildStageVarName} is: ${buildStageName}"
+
+        //
+    }
+    catch (Exception e){
+        echo "Scan failed"
+        throw e
+    }
 }
