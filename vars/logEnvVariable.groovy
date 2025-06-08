5f449e2a22eb632da32fe23c9fbcfcaac3cdcc6b
carmen-chau <carmenjchau@gmail.com>
2025-06-08 13:09:47 -0400

    corrected syntax

--- vars/logEnvVariable.groovy
+++ vars/logEnvVariable.groovy
@@ -1,10 +1,10 @@
-void call(String gitURLVarName, String buildStageVarName) {
+void call(String gitUrlVarName, String buildStageVarName) {
     try {
 
         // Variable reassignment, logging values for verification
         def gitRepoName = env[gitUrlVarName]
         def buildStageName = env[buildStageVarName]
-        echo "Inside the helper script, the value of ${gitURLVarName} is: ${gitRepoName}"
+        echo "Inside the helper script, the value of ${gitUrlVarName} is: ${gitRepoName}"
         echo "Inside the helper script, the value of ${buildStageVarName} is: ${buildStageName}"
 
         //
