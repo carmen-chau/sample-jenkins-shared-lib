4487b9f3999c0dd84280139558adb2a19bd2dad9
carmen-chau <carmenjchau@gmail.com>
2025-06-08 13:53:45 -0400

    changed variable name and value

--- vars/envVariables.groovy
+++ vars/envVariables.groovy
@@ -1,7 +1,7 @@
 // A file that attempts to assign/create new env variables
 void call(){
     try {
-        env.FAV_FRUIT = "strawberry"
+        env.nexusIQStage = "build"
     }
     catch (Exception e){
         echo "Variable assignment or creation has failed"
