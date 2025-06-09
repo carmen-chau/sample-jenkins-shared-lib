c287a04fadc0c7a3653a5b5032ba26f96874c3f9
carmen-chau <carmenjchau@gmail.com>
2025-06-09 00:02:03 -0400

    added string split method

--- vars/logEnvVariable.groovy
+++ vars/logEnvVariable.groovy
@@ -2,12 +2,16 @@ void call(String gitUrlVarName, String buildStageVarName) {
     try {
 
         // Variable reassignment, logging values for verification
+        // Note: Even when the variable is null, this shouldn't throw any errors nor exceptions
         def gitRepoName = env[gitUrlVarName]
         def buildStageName = env[buildStageVarName]
         echo "Inside the helper script, the value of ${gitUrlVarName} is: ${gitRepoName}"
         echo "Inside the helper script, the value of ${buildStageVarName} is: ${buildStageName}"
 
-        //
+        // Attempt to call a string function for variable gitRepoName
+        // Key Idea: If the variable stores a null value, this should throw an exception
+        def revisedGitRepoName = gitRepoName.split(" ")
+        echo "Inside the helper script, the value of revisedGitRepoName is: ${revisedGitRepoName}"
     }
     catch (Exception e){
         echo "Scan failed"
